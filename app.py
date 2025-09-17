2f3344f5930627be0ba1734e05caded1598ea271
Angélico Sala 406 <ansala406@usantotomas.red>
2025-09-17 15:50:17 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pickle
+from sklearn.linear_model import LinearRegression
+import sklearn as sk
+import numpy as np
+
+st.title("Primer modelo")
+
+with open("model.pickle", "rb") as f:
+    modelo = pickle.load(f)
+
+# seleccionar la educación
+
+educ = st.slider("Años de educación", 0, 25)
+
+# seleccionar la experiencia
+
+exper = st.slider("Años de experiencia", 0, 45)
+
+vec = np.array([[educ, exper]])
+
+if st.button("Predecir"):
+    res = modelo.predict(vec)[0]
+    st.write(f"Su salario en 1976 sería de {round(res,1)} por hora.")
+
